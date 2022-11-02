b7008808bb9a61f0e4ef0688198dcda5a82fa8c1
Ranggaalan <95030340+Ranggaalan@users.noreply.github.com>
2022-11-02 18:55:53 +0700

    Tugas 7

---
+++ Tugas_7.sh
@@ -0,0 +1,22 @@
+#!bin/bash/sh
+
+# Mendeklarasikan fungsi
+function panjang {
+	echo "Masukkan Panjang : "
+	read  panjang
+}
+function lebar {
+	echo "Masukkan Lebar : "
+	read lebar
+}
+function luas {
+	let luas=$panjang*$lebar
+	echo "Luas Persegi :"
+	echo $luas
+}
+
+# Memanggil fungsi
+panjang
+lebar
+luas
+
